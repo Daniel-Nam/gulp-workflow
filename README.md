0dc4eea676d4d5231bf0d492b04956d44eaace30
Nam-dev-web <hoangnambdg30@gmail.com>
2022-03-25 20:32:07 +0700

    push

---
+++ README.md
@@ -0,0 +1,26 @@
+# Basic Gulp-Workflow for beginners 🔥🔥🔥
+
+## ⚙️ Technology Using
+
+-   `gulp`
+-   `gulp-babel`
+-   `autoprefixer`
+-   `gulp-postcss`
+-   `cssnano`
+-   `gulp-pug`
+-   `gulp-sass`
+-   `sass`
+-   `browser-sync`
+
+### 🤖 Require
+
+-   ✅ _Installed Nodejs_
+-   ✅ _Download all Dependencies before using_
+-   💀 **Don't edit anything in the public folder, you can change the images folder if you like :v**
+
+### 📃 Usage
+
+```
+npm install - download Dependencies
+gulp - start app
+```
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const { watch, series, dest, src } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const pug = require('gulp-pug')
+
+function sassTask() {
+	return src('src/sass/**/*.scss')
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('public/css'))
+}
+
+function jsTask() {
+	return src('src/scripts/**/*.js')
+		.pipe(
+			babel({
+				presets: ['@babel/preset-env'],
+			})
+		)
+		.pipe(dest('public/scripts'))
+}
+
+function pugTask() {
+	return src('src/*.pug')
+		.pipe(
+			pug({
+				pretty: true,
+			})
+		)
+		.pipe(dest('public'))
+}
+
+function browserSyncServer(cb) {
+	browserSync.init({
+		server: {
+			baseDir: 'public',
+		},
+	})
+	cb()
+}
+
+function browserSyncReload(cb) {
+	browserSync.reload()
+	cb()
+}
+
+function watchTask() {
+	watch('*.html', browserSyncReload)
+	watch(
+		['src/**/*.pug', 'src/sass/**/*.scss', 'src/scripts/**/*.js'],
+		series(pugTask, sassTask, jsTask, browserSyncReload)
+	)
+}
+
+exports.default = series(
+	pugTask,
+	sassTask,
+	jsTask,
+	browserSyncServer,
+	watchTask
+)
